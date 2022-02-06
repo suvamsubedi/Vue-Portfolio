c05516bf0e9d5d3e83dfff5634b6e30cbd29750e
Suvam Subedi <suvamsubedi@gmail.com>
2022-02-06 13:58:33 +0545

    Test commit

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>I am contact</h1>
+    <h1>This is contact</h1>
 </template>
 
 <style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,29 @@
 <template>
-    <div class="footer__div">
-        <p>Designed and Developed By Suvam</p>
-        <p>Copyright &copy; 2022. [this is the demo]</p>    
-</div>
+  <div class="footer__div">
+    <p>Designed and Developed By Suvam</p>
+    <p>Copyright &copy; 2022. [this is the demo]</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name : 'Footer'
-}
+  name: "Footer",
+};
 </script>
 
 <style>
-   .footer__div {
-    border-top: 1px solid lightgray;
-    padding: 20px;
-    background-color: #f7f7f7; 
-    text-align: center;
-    position: fixed;
-    bottom: 0;
-    width: 100%;
+.footer__div {
+  border-top: 1px solid lightgray;
+  padding: 20px;
+  background-color: #f7f7f7;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 }
 
-.footer__div > p{
-    padding: 5px;
-    font-size: 15px;
-
+.footer__div > p {
+  padding: 5px;
+  font-size: 15px;
 }
-
 </style>
