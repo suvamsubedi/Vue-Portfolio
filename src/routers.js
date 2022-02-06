5a7438d35dad04df48895296c921cd6e3ddafaf2
MrVAms <suvamsubedi@gmail.com>
2022-02-06 13:53:28 +0545

    Added About page.

--- src/routers.js
+++ src/routers.js
@@ -1,34 +1,38 @@
-import Contact from './components/Contact.vue'
-import Skills from './components/Skills.vue'
-import BodyContent from './components/BodyContent.vue'
-import {createRouter, createWebHistory} from 'vue-router'
+import Contact from "./components/Contact.vue";
+import Skills from "./components/Skills.vue";
+import BodyContent from "./components/BodyContent.vue";
+import About from "./components/About.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-const routes =[
-    {
-        name: 'Home',
-        component : BodyContent,
-        path:'/',
-    },
+const routes = [
+  {
+    name: "Home",
+    component: BodyContent,
+    path: "/",
+  },
 
-    {
-        name: 'Contact',
-        component : Contact,
-        path:'/contact',
-    },
+  {
+    name: "Contact",
+    component: Contact,
+    path: "/contact",
+  },
 
-    {
-        name: 'Skills',
-        component: Skills,
-        path: "/skills",
-    }
+  {
+    name: "Skills",
+    component: Skills,
+    path: "/skills",
+  },
+
+  {
+    name: "About",
+    component: About,
+    path: "/about",
+  }
 ];
 
-
-
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
